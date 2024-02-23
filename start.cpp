8f4d5f6b45e519c4edf09ce2207fc023b33533e3
idflool <150003344+idflool@users.noreply.github.com>
2024-02-23 11:49:19 +0300

    Add files via upload

---
+++ start.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+
+{
+	setlocale(LC_ALL, "rus");
+	/*int R0;
+	int R1;
+	int R2;
+	int R3;
+	cin >> R1;
+	cin >> R2;
+	cin >> R3;
+	cout << 1 / R1 + 1 / R2 + 1 / R3;
+*/
+
+	//int L;
+	//int S;
+	//int R;
+	//cin >> L;
+	//const float Pi = 3.14;
+	//cout << "R=" << L / (2 * Pi);
+	//R = L / (2 * Pi);
+	//cout << endl <<"s=" << R * 2 * Pi;
+
+
+	double v, t, a;
+	cin >> v >> a >> t;
+	cout << " s=" << v * t + (a * t *2) / 2;
+
+
+
+
+
+
+
+
+
+
+}
