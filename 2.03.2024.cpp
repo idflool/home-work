b8ab4a33e3ac6e7d2e60b42f976b7a0014328a39
idflool <150003344+idflool@users.noreply.github.com>
2024-03-03 19:21:50 +0300

    Add files via upload

---
+++ 2.03.2024.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	/*int a, b, c, d, e, f, j,first,second;
+	cin >> a;
+	b = a / 100000;
+	a = b;
+	c = a / 10000;
+	a = c;
+	d = a / 1000;
+	a = d;
+	e = a / 100;
+	a = e;
+	f = a / 10;
+	a = f;
+	j = a / 1;
+	first = d + c + d;
+	second = e + f + j;
+	if ( first == second) {
+		cout << "luck";
+	}
+	else {
+		cout << "lose";
+	}*/
+
+	/*int a, b, c, d, e;
+	cin >> a;
+	if (a / 1000 < 1)
+	{
+		cout << "eroor ";
+	}
+	else if (a / 1000 > 9)
+	{
+		cout << "error ";
+	}
+	else
+	{
+		cout << "yes ";
+	}
+	b = a / 10;
+	a = b;
+	c = a / 100;
+	a = c;
+	d = a / 1000;
+	a = d;
+	e = a;
+	cout << c << b << e << d;*/
+
+
+	/*int a, b, c, d, e, f,j, max;
+	cin >> a >> b >> c >> d >> e >> f>> j;
+	if (a > b)
+	{
+		max = a;
+	}
+	else
+	{
+		max = b;
+	}
+	if (c > max)
+	{
+		max = c;
+	}
+	if (d > max)
+	{
+		max = d;
+	}
+	if (e > max)
+	{
+			max = e;
+	}
+	if (f > max)
+	{
+		max = f;
+	}
+	if (j > max)
+	{
+		max = j;
+	}
+	cout << max;*/
+
+
+
+
+
+}
+
+
+
+
