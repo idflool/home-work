cb30ef1ddb91e694a9d3978a80d39b352e5b2d2e
idflool <150003344+idflool@users.noreply.github.com>
2024-03-03 18:50:53 +0300

    Add files via upload

---
+++ 29.02.2024.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	//float a;
+	//cin >> a;
+	//cout << " min " << a / 60 << endl;
+	//cout << "chas " << a / 60 / 60 << endl;
+	//cout << "sek " << a;
+
+
+
+	/*double a;
+	int dollar, cent;
+	cin >> a;
+	dollar = a;
+	cent = (a - dollar) * 100;
+	cout << dollar << endl << cent;*/
+
+
+	//int a,d, w;
+	//cin >> a;
+	//w = a / 7;
+	//d = a % 7;
+	//cout << w << endl << d;
+
+
+	double t;
+	float s;
+	int m, sek, time;
+	cout << "Введите длину дистанции (метров) = "; cin >> s; cout<< endl;
+	cout << "Введите время (мин.сек) = "; cin >> t; cout << endl ;
+	cout << "Дистанция: " << s << endl;
+	m = t;
+	sek = (t - m) * 100;
+	time = m * 60 + sek;
+	cout << "Время: " << t << m * 60 + sek << endl;
+	cout << "Вы бежали со скоростью " << s / time * 3.6;
+
+}
+
+
+
+
