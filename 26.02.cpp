647648146e8fce279da985d24f87bf7f7347e48d
idflool <150003344+idflool@users.noreply.github.com>
2024-03-12 21:33:27 +0300

    Add files via upload

---
+++ 26.02.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	//1
+	/*int a,c=0;
+	cin >> a;
+	for (int i = a; i < 500; i++) {
+		c += i;
+		cout << c << endl;
+	}*/
+
+	//2
+	//int a, b;
+	//cin >> a >> b;
+	//cout << pow(a, b);
+
+	//3
+	/*int avg, sum = 0;
+	for (int i = 0; i <= 1000; i++) {
+		sum +=i;
+	}
+	cout << sum / 1000;*/
+
+	//4
+	//int a;
+	//cin >> a;
+	//for (int i = a; i <= 20; i++) {
+	//	a = a * i;
+	//}
+	//cout << a << endl;
+
+
+
+	//5
+	//int a;
+	//cin >> a;
+	//for (int i = 1; i <= 9; i++) {
+	//	cout << a * i << endl;
+	//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
